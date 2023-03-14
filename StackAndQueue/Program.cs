8c85bcaf5bfed1e5da30cb3f399fa77906791168
Vivek Verma <vverma240420@gmail.com>
2023-03-14 17:23:44 +0530

    Add Push Operation of stack

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace StackAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Programs");
+            Console.WriteLine("Choose the option of the program want to perform");
+            Console.WriteLine("1. Push Operation");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Stack stack = new Stack();
+                    stack.Push(70);
+                    stack.Push(30);
+                    stack.Push(56);
+                    stack.Display();
+                    break;
+                default:
+                    Console.WriteLine("Please choose the correct option");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StackAndQueue/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class Stack
+    {
+
+        public Node top;
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if (top == null)
+            {
+                top = newNode;
+                Console.WriteLine("{0} Node is Inserted into Stack", newNode.data);
+            }
+            else
+            {
+                newNode.next = top;
+                top = newNode;
+                Console.WriteLine("{0} Node is Inserted into Stack", newNode.data);
+            }
+        }
+
+        public void Display()
+
+        {
+            Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("\nStack is Empty");
+            }
+            else
+            {
+                Console.WriteLine("\nDisplaying the Nodes");
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
