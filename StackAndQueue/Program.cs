9932b3a81d1def3e19c7acf162a2e2e7ad5d3499
Vivek Verma <vverma240420@gmail.com>
2023-03-14 17:43:04 +0530

    Add Enqueue Operation of Queue

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to Stack and Queue Programs");
             Console.WriteLine("Choose the option of the program want to perform");
-            Console.WriteLine("1. Push Operation \n2. Peek and Pop Operation");
+            Console.WriteLine("1. Push Operation \n2. Peek and Pop Operation \n3. Enqueue Operation");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -15,7 +15,7 @@
                     stack.Push(70);
                     stack.Push(30);
                     stack.Push(56);
-                    stack.Display();
+                    stack.Display();//56 30 70 display same as AddFirst
                     break;
                 case 2:
                     Stack popStack = new Stack();
@@ -28,12 +28,20 @@
                     //Console.WriteLine(popStack.IsEmpty());
                     popStack.Display();
                     break;
+                case 3:
+                    Queue queue = new Queue();
+                    queue.Enqueue(56);
+                    queue.Enqueue(30);
+                    queue.Enqueue(70);
+                    queue.Display();//56 30 70 same as AddLast Head marked on the first node
+                    break;
                 default:
                     Console.WriteLine("Please choose the correct option");
                     break;
             }
 
             Console.ReadLine();
+
         }
     }
 }
---
+++ StackAndQueue/Queue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class Queue
+    {
+        public Node head;
+        public void Enqueue(int data)
+        {
+            Node new_node = new Node(data);
+            if (head == null)
+            {
+                head = new_node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = new_node;
+            }
+        }
+        public void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                Console.WriteLine("\nDisplaying the Queue");
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
